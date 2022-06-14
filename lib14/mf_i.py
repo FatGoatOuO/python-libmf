dc03528fbe3d148e4899c1f010ed6536ce662d75
FatGoatOuO <103632773+FatGoatOuO@users.noreply.github.com>
2022-06-14 22:32:15 +0800

    Update mf_i.py

--- lib14/mf_i.py
+++ lib14/mf_i.py
@@ -32,101 +32,6 @@ from sklearn.linear_model import Lasso,LassoCV,LassoLarsCV
 import warnings; warnings.simplefilter('ignore')
 #%matplotlib auto
 
-def parse(path):
-  g = gzip.open(path, 'rb')
-  for l in g:
-    yield json.loads(l)
-
-def getDF(path):
-  i = 0
-  df = {}
-  for d in parse(path):
-    df[i] = d
-    i += 1
-  return pd.DataFrame.from_dict(df, orient='index')
-
-#with open('Appliances_5_test', 'rb') as f_in, gzip.open('test.json.gz', 'wb') as f_out:
-  #shutil.copyfileobj(f_in, f_out)
-df = getDF('Appliances_5.json.gz') #讀檔
-
-df.insert(df.shape[1], 'word', 0) # 增加一欄位用以儲存評論之名詞
-
-userNo = len(np.unique(df.reviewerID)) #使用者總數
-itemNo = len(np.unique(df.asin)) #商品總數
-D = 29 #隱含特徵數
-T = 36 #時間點數
-merge = 2 #每幾個時間點合併 
-user_list = np.unique(df.reviewerID) #取出所有不同使用者
-item_list = np.unique(df.asin) #取出所有不同商品
-rating = np.zeros((itemNo, userNo)) #創建矩陣
-user_list = pd.Series(user_list.tolist())
-item_list = pd.Series(item_list.tolist())
-
-df.reviewText = df.reviewText.astype(str)
-
-bow = []
-stop_words = set(stopwords.words('english')) #停用詞
-for i in range(len(df)): #建立包含所有評論內名詞的詞袋
-
-    text= df.reviewText[i].lower()
-    tokenizer = RegexpTokenizer(r'\w+')
-    tokens = nltk.word_tokenize(text)
-    tags = nltk.pos_tag(tokens)
-    lemmatizer = WordNetLemmatizer()
-    nouns = [word for word,pos in tags if (pos == 'NN' or pos == 'NNP' or pos == 'NNS' or pos == 'NNPS')] #留下名詞
-    nouns = [w for w in nouns if not w.lower() in stop_words] #去除停用詞
-    nouns = [w for w in nouns if w.isalpha()] #去除標點符號
-    nouns = [lemmatizer.lemmatize(w) for w in nouns] #詞幹化
-    df.word[i] = nouns
-    bow += nouns
-bow = np.unique(bow) #留下不重複的
-
-i_tfidf = pd.DataFrame() #算I矩陣(TF-IDF)
-for i in range(len(df)):
-    vectorizer = TfidfVectorizer(stop_words='english', vocabulary = bow)
-    tfidf = vectorizer.fit_transform([df.reviewText[i]])
-    result = pd.DataFrame(tfidf.toarray(), columns=vectorizer.vocabulary_)
-    i_tfidf = i_tfidf.append(result, ignore_index=True)
-i_tfidf = np.transpose(i_tfidf)
-
-model = NMF(n_components = D, random_state = 5)
-model.fit(i_tfidf)
-nmf_features = model.transform(i_tfidf)
-h = nmf_features
-w = model.components_
-
-temp = pd.DataFrame(columns = ['asin', 'userID', 'review']) #把同一物品/使用者的評論單詞放在一起變新的評論
-temp.asin = df.asin
-temp.review = df.word
-temp.userID = df.reviewerID
-psiJ = temp.groupby(by = 'asin')['review'].sum() #同物品
-space = ' '
-for i  in range(len(psiJ)):
-    psiJ[i] = space.join(psiJ[i])
-
-psiI = temp.groupby(by = 'userID')['review'].sum() #同使用者
-space = ' '
-for i  in range(len(psiI)):
-    psiI[i] = space.join(psiI[i])
-
-f_item = pd.DataFrame() #算psi的TF-IDF
-for i in range(len(psiJ)):
-    vectorizer = TfidfVectorizer(stop_words='english', vocabulary = bow)
-    tfidf = vectorizer.fit_transform([psiJ[i]])
-    result = pd.DataFrame(tfidf.toarray(), columns=vectorizer.get_feature_names())
-    f_item = f_item.append(result, ignore_index=True)
-
-
-f_user = pd.DataFrame() #算psi的TF-IDF
-for i in range(len(psiI)):
-    vectorizer = TfidfVectorizer(stop_words='english', vocabulary = bow)
-    tfidf = vectorizer.fit_transform([psiI[i]])
-    result = pd.DataFrame(tfidf.toarray(), columns=vectorizer.get_feature_names())
-    f_user = f_user.append(result, ignore_index=True)
-
-v_hat = np.dot(f_item, h) #投射到U_hat和V_hat
-u_hat = np.dot(f_user, h)
-
 
 if "LIBMF_OBJ" in os.environ:
     print("Using compiled .so file specified in LIBMF_OBJ:")
@@ -206,6 +111,201 @@ options_ptr = ctypes.POINTER(MFParam)
 
 
 class MF(object):
+	
+    def uhat(self):
+        def parse(path):
+          g = gzip.open(path, 'rb')
+          for l in g:
+            yield json.loads(l)
+
+        def getDF(path):
+          i = 0
+          df = {}
+          for d in parse(path):
+            df[i] = d
+            i += 1
+          return pd.DataFrame.from_dict(df, orient='index')
+
+        #with open('Appliances_5_test', 'rb') as f_in, gzip.open('test.json.gz', 'wb') as f_out:
+          #shutil.copyfileobj(f_in, f_out)
+        df = getDF('Appliances_5.json.gz') #讀檔
+
+        df.insert(df.shape[1], 'word', 0) # 增加一欄位用以儲存評論之名詞
+
+        userNo = len(np.unique(df.reviewerID)) #使用者總數
+        itemNo = len(np.unique(df.asin)) #商品總數
+        D = 29 #隱含特徵數
+        T = 36 #時間點數
+        merge = 2 #每幾個時間點合併 
+        user_list = np.unique(df.reviewerID) #取出所有不同使用者
+        item_list = np.unique(df.asin) #取出所有不同商品
+        rating = np.zeros((itemNo, userNo)) #創建矩陣
+        user_list = pd.Series(user_list.tolist())
+        item_list = pd.Series(item_list.tolist())
+
+        df.reviewText = df.reviewText.astype(str)
+
+        bow = []
+        stop_words = set(stopwords.words('english')) #停用詞
+        for i in range(len(df)): #建立包含所有評論內名詞的詞袋
+
+            text= df.reviewText[i].lower()
+            tokenizer = RegexpTokenizer(r'\w+')
+            tokens = nltk.word_tokenize(text)
+            tags = nltk.pos_tag(tokens)
+            lemmatizer = WordNetLemmatizer()
+            nouns = [word for word,pos in tags if (pos == 'NN' or pos == 'NNP' or pos == 'NNS' or pos == 'NNPS')] #留下名詞
+            nouns = [w for w in nouns if not w.lower() in stop_words] #去除停用詞
+            nouns = [w for w in nouns if w.isalpha()] #去除標點符號
+            nouns = [lemmatizer.lemmatize(w) for w in nouns] #詞幹化
+            df.word[i] = nouns
+            bow += nouns
+        bow = np.unique(bow) #留下不重複的
+
+        i_tfidf = pd.DataFrame() #算I矩陣(TF-IDF)
+        for i in range(len(df)):
+            vectorizer = TfidfVectorizer(stop_words='english', vocabulary = bow)
+            tfidf = vectorizer.fit_transform([df.reviewText[i]])
+            result = pd.DataFrame(tfidf.toarray(), columns=vectorizer.vocabulary_)
+            i_tfidf = i_tfidf.append(result, ignore_index=True)
+        i_tfidf = np.transpose(i_tfidf)
+
+        model = NMF(n_components = D, random_state = 5)
+        model.fit(i_tfidf)
+        nmf_features = model.transform(i_tfidf)
+        h = nmf_features
+        w = model.components_
+
+        temp = pd.DataFrame(columns = ['asin', 'userID', 'review']) #把同一物品/使用者的評論單詞放在一起變新的評論
+        temp.asin = df.asin
+        temp.review = df.word
+        temp.userID = df.reviewerID
+        psiJ = temp.groupby(by = 'asin')['review'].sum() #同物品
+        space = ' '
+        for i  in range(len(psiJ)):
+            psiJ[i] = space.join(psiJ[i])
+
+        psiI = temp.groupby(by = 'userID')['review'].sum() #同使用者
+        space = ' '
+        for i  in range(len(psiI)):
+            psiI[i] = space.join(psiI[i])
+
+        f_item = pd.DataFrame() #算psi的TF-IDF
+        for i in range(len(psiJ)):
+            vectorizer = TfidfVectorizer(stop_words='english', vocabulary = bow)
+            tfidf = vectorizer.fit_transform([psiJ[i]])
+            result = pd.DataFrame(tfidf.toarray(), columns=vectorizer.get_feature_names())
+            f_item = f_item.append(result, ignore_index=True)
+
+
+        f_user = pd.DataFrame() #算psi的TF-IDF
+        for i in range(len(psiI)):
+            vectorizer = TfidfVectorizer(stop_words='english', vocabulary = bow)
+            tfidf = vectorizer.fit_transform([psiI[i]])
+            result = pd.DataFrame(tfidf.toarray(), columns=vectorizer.get_feature_names())
+            f_user = f_user.append(result, ignore_index=True)
+
+        v_hat = np.dot(f_item, h) #投射到U_hat和V_hat
+        u_hat = np.dot(f_user, h)
+	return u_hat
+
+    def vhat(self):
+        def parse(path):
+          g = gzip.open(path, 'rb')
+          for l in g:
+            yield json.loads(l)
+
+        def getDF(path):
+          i = 0
+          df = {}
+          for d in parse(path):
+            df[i] = d
+            i += 1
+          return pd.DataFrame.from_dict(df, orient='index')
+
+        #with open('Appliances_5_test', 'rb') as f_in, gzip.open('test.json.gz', 'wb') as f_out:
+          #shutil.copyfileobj(f_in, f_out)
+        df = getDF('Appliances_5.json.gz') #讀檔
+
+        df.insert(df.shape[1], 'word', 0) # 增加一欄位用以儲存評論之名詞
+
+        userNo = len(np.unique(df.reviewerID)) #使用者總數
+        itemNo = len(np.unique(df.asin)) #商品總數
+        D = 29 #隱含特徵數
+        T = 36 #時間點數
+        merge = 2 #每幾個時間點合併 
+        user_list = np.unique(df.reviewerID) #取出所有不同使用者
+        item_list = np.unique(df.asin) #取出所有不同商品
+        rating = np.zeros((itemNo, userNo)) #創建矩陣
+        user_list = pd.Series(user_list.tolist())
+        item_list = pd.Series(item_list.tolist())
+
+        df.reviewText = df.reviewText.astype(str)
+
+        bow = []
+        stop_words = set(stopwords.words('english')) #停用詞
+        for i in range(len(df)): #建立包含所有評論內名詞的詞袋
+
+            text= df.reviewText[i].lower()
+            tokenizer = RegexpTokenizer(r'\w+')
+            tokens = nltk.word_tokenize(text)
+            tags = nltk.pos_tag(tokens)
+            lemmatizer = WordNetLemmatizer()
+            nouns = [word for word,pos in tags if (pos == 'NN' or pos == 'NNP' or pos == 'NNS' or pos == 'NNPS')] #留下名詞
+            nouns = [w for w in nouns if not w.lower() in stop_words] #去除停用詞
+            nouns = [w for w in nouns if w.isalpha()] #去除標點符號
+            nouns = [lemmatizer.lemmatize(w) for w in nouns] #詞幹化
+            df.word[i] = nouns
+            bow += nouns
+        bow = np.unique(bow) #留下不重複的
+
+        i_tfidf = pd.DataFrame() #算I矩陣(TF-IDF)
+        for i in range(len(df)):
+            vectorizer = TfidfVectorizer(stop_words='english', vocabulary = bow)
+            tfidf = vectorizer.fit_transform([df.reviewText[i]])
+            result = pd.DataFrame(tfidf.toarray(), columns=vectorizer.vocabulary_)
+            i_tfidf = i_tfidf.append(result, ignore_index=True)
+        i_tfidf = np.transpose(i_tfidf)
+
+        model = NMF(n_components = D, random_state = 5)
+        model.fit(i_tfidf)
+        nmf_features = model.transform(i_tfidf)
+        h = nmf_features
+        w = model.components_
+
+        temp = pd.DataFrame(columns = ['asin', 'userID', 'review']) #把同一物品/使用者的評論單詞放在一起變新的評論
+        temp.asin = df.asin
+        temp.review = df.word
+        temp.userID = df.reviewerID
+        psiJ = temp.groupby(by = 'asin')['review'].sum() #同物品
+        space = ' '
+        for i  in range(len(psiJ)):
+            psiJ[i] = space.join(psiJ[i])
+
+        psiI = temp.groupby(by = 'userID')['review'].sum() #同使用者
+        space = ' '
+        for i  in range(len(psiI)):
+            psiI[i] = space.join(psiI[i])
+
+        f_item = pd.DataFrame() #算psi的TF-IDF
+        for i in range(len(psiJ)):
+            vectorizer = TfidfVectorizer(stop_words='english', vocabulary = bow)
+            tfidf = vectorizer.fit_transform([psiJ[i]])
+            result = pd.DataFrame(tfidf.toarray(), columns=vectorizer.get_feature_names())
+            f_item = f_item.append(result, ignore_index=True)
+
+
+        f_user = pd.DataFrame() #算psi的TF-IDF
+        for i in range(len(psiI)):
+            vectorizer = TfidfVectorizer(stop_words='english', vocabulary = bow)
+            tfidf = vectorizer.fit_transform([psiI[i]])
+            result = pd.DataFrame(tfidf.toarray(), columns=vectorizer.get_feature_names())
+            f_user = f_user.append(result, ignore_index=True)
+
+        v_hat = np.dot(f_item, h) #投射到U_hat和V_hat
+        u_hat = np.dot(f_user, h)
+	return v_hat
+	
     def __init__(self, *args, **kwargs):
         self.model = None
         self._options = MFParam()
@@ -315,8 +415,8 @@ class MF(object):
     def q_factors(self):
         if self.model is None:
             return LookupError("no model data is saved, try running model.mf_fit(...) first")
-        u_hat = u_hat.reshape(self.model.n * self.model.k)
-        out = u_hat
+        uhat = uhat.reshape(self.model.n * self.model.k)
+        out = uhat
         out = out.astype(np.float32)
         mf.get_Q(ctypes.c_void_p(out.ctypes.data), ctypes.byref(self.model))
         return out.reshape((self.model.n, self.model.k))
@@ -324,8 +424,8 @@ class MF(object):
     def p_factors(self):
         if self.model is None:
             return LookupError("no model data is saved, try running model.mf_fit(...) first")
-        v_hat = v_hat.reshape(self.model.m * self.model.k)
-        out = v_hat
+        vhat = vhat.reshape(self.model.m * self.model.k)
+        out = vhat
         out = out.astype(np.float32)
         mf.get_P(ctypes.c_void_p(out.ctypes.data), ctypes.byref(self.model))
         return out.reshape((self.model.m, self.model.k))
