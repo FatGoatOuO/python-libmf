d49b0963f70be37ae78afd24e9e684d87379c13b
FatGoatOuO <103632773+FatGoatOuO@users.noreply.github.com>
2022-09-19 19:44:19 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ def gen_test_suite():
     return test_suite
 
 
-setup(name='lib-app5',
-      packages=['lib-app5'],
+setup(name='lib-mag5',
+      packages=['lib-mag5'],
       version='0.9.2',
       test_suite='setup.gen_test_suite',
       description='python bindings to libmf',
